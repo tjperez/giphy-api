8c6c4d743a55021cf7c7968f9de9b9389bbd6814
tjlinares <tjlinares23@gmail.com>
2019-06-10 12:57:35 -0400

    Updates

---
+++ precache-manifest.46e0f968bde3abcd10c593bfeeada727.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51269d5eb646bee15fd18b0a4e379cb2",
+    "url": "/giphy-api/index.html"
+  },
+  {
+    "revision": "9819b89dbad1d5226334",
+    "url": "/giphy-api/static/css/main.b300f62c.chunk.css"
+  },
+  {
+    "revision": "658c859d6a512586c843",
+    "url": "/giphy-api/static/js/2.a8c9abaa.chunk.js"
+  },
+  {
+    "revision": "9819b89dbad1d5226334",
+    "url": "/giphy-api/static/js/main.ccf7ec4f.chunk.js"
+  },
+  {
+    "revision": "7f26869ae7d17d36a59e",
+    "url": "/giphy-api/static/js/runtime~main.c9e52869.js"
+  }
+]);
